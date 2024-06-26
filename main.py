5578ea35049953cf488d5327014f0361137ac758
bearspi <bearsquaduzgun@gmail.com>
2024-06-26 21:45:40 +0300

    Fixed Intents

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ token=input("Token")
 
 
     
-intents = Intents.DEFAULT
+intents = Intents.DEFAULT  | Intents.MESSAGE_CONTENT
 activty = Activity.create("Coming back?", url= "Developed by (rembeber to fill in this gap)")
 bot = Client(intents= intents, activity= activty, token= token)
 prefixed_commands.setup(bot, default_prefix="l!")
