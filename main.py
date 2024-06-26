0b4f910de49155d9926e45ced2531b26b17d6b14
bearspi <bearsquaduzgun@gmail.com>
2024-06-26 21:36:04 +0300

    Basic Commands added

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import discord
-from discord.ext import commands
+from interactions import Client, Intents, Activity
+
+from interactions.ext import prefixed_commands
 token=input("Token")
 
+
     
+intents = Intents.DEFAULT
+activty = Activity.create("Coming back?", url= "Developed by (rembeber to fill in this gap)")
+bot = Client(intents= intents, activity= activty, token= token)
+prefixed_commands.setup(bot, default_prefix="l!")
+
+bot.load_extension("test")
+bot.start(token)
