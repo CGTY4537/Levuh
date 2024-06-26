83d5f5c0fea230eeebcec650489795559b7e33b3
novissima <cinaregemen@gmail.com>
2024-06-26 21:27:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import discord
-from discord.ext import commands
-token=input("Token")
-def fonks():
-    return True
-    
+import interactions
+bot = interactions.Client()
+
+@interactions.listen()
+async def on_startup():
+    print("Bot is ready!")
+
+bot.start("MTI1NDQyNjI1MzE4MDY2NTg1Ng.Gy06aj.lwAHi97ACPVVzJ4h_wwEnYQ539M-KiMbVvvvlA")
