f8e10042bf82d2393971b2a1a966cb81bc216f7b
novissima <cinaregemen@gmail.com>
2024-06-29 14:38:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import interactions
-bot = interactions.Client()
-
-@interactions.listen()
-async def on_startup():
-    print("Bot is ready!")
-
-bot.start("MTI1NDQyNjI1MzE4MDY2NTg1Ng.Gy06aj.lwAHi97ACPVVzJ4h_wwEnYQ539M-KiMbVvvvlA")
 from interactions import Client, Intents, Activity
 
 from interactions.ext import prefixed_commands
@@ -29,4 +21,5 @@ bot = Client(intents= intents, activity= activty, token= token)
 prefixed_commands.setup(bot, default_prefix="l!")
 
 bot.load_extension("test")
+bot.start(token)
 bot.start(token)
